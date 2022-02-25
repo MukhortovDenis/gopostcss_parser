ed6b35ebfe4d722c6eb34d36ba69346766694874
Denis Muhortov <muhortovdenis48@gmail.com>
2022-02-25 23:53:12 +0300

    add createTokens

--- parser.go
+++ parser.go
@@ -7,6 +7,13 @@ import (
 	"go.uber.org/zap"
 )
 
+var (
+	newline       = []byte("\n")
+	selectorID    = []byte("#")
+	selectorClass = []byte(".")
+	selectorAll   = []byte("*")
+)
+
 type AST struct {
 	Tokens []*Token
 	logger *zap.Logger
@@ -38,11 +45,65 @@ func (ast *AST) scanFile(filename string) error {
 	if err != nil {
 		ast.logger.Error("error", zap.Error(err))
 	}
+
+	var i int
+	cache := map[int][]byte{}
+
 	sc := bufio.NewScanner(file)
-	_, _ = createToken(sc)
+	for sc.Scan() {
+		cache[i] = sc.Bytes()
+		i++
+	}
+
+	_, _ = ast.createTokens(cache)
 	return nil
 }
 
-func createToken(sc *bufio.Scanner) ([]*Token, error) {
-	return nil, nil
+func (ast *AST) createTokens(cache map[int][]byte) ([]*Token, error) {
+	tokens := []*Token{}
+	for i, key := range cache {
+		switch key[0] {
+
+		case newline[0]:
+			continue
+
+		case selectorAll[0]:
+			tokenAll, err := tokenSelectorAll(i, cache)
+			if err != nil {
+				ast.logger.Error("error", zap.Error(err))
+			}
+			tokens = append(tokens, tokenAll)
+
+		case selectorID[0]:
+			tokenID, err := tokenSelectorID(i, cache)
+			if err != nil {
+				ast.logger.Error("error", zap.Error(err))
+			}
+			tokens = append(tokens, tokenID)
+
+		case selectorClass[0]:
+			tokenClass, err := tokenSelectorClass(i, cache)
+			if err != nil {
+				ast.logger.Error("error", zap.Error(err))
+			}
+			tokens = append(tokens, tokenClass)
+
+		default:
+			tokenTag, err := tokenSelectorTag(i, cache)
+			if err != nil {
+				ast.logger.Error("error", zap.Error(err))
+			}
+			tokens = append(tokens, tokenTag)
+		}
+
+	}
+	return tokens, nil
 }
+
+func tokenSelectorID(i int, cache map[int][]byte) (*Token, error)
+
+func tokenSelectorClass(i int, cache map[int][]byte) (*Token, error)
+
+func tokenSelectorAll(i int, cache map[int][]byte) (*Token, error)
+
+func tokenSelectorTag(i int, cache map[int][]byte) (*Token, error)
